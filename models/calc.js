8a313896f38bcc0d8eb6cc7e1f2d3c4a1fcb8672
jennysol <jennyssoliver@gmail.com>
2023-09-26 20:29:08 -0300

    update: remove file test

--- models/calc.js
+++
@@ -1,9 +0,0 @@
-function sum(a, b) {
-  if (typeof a !== "number") {
-    return "error";
-  }
-
-  return a + b;
-}
-
-exports.sum = sum;
--- pages/api/status.js
+++
@@ -1,5 +0,0 @@
-function status(request, response) {
-  response.status(200).json({ message: "Status successfully!" });
-}
-
-export default status;
