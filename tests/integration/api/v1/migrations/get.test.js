05ca45862d7ed7aa9997d63bcd5cda1d5f9b53c4
jennysol <jennyssoliver@gmail.com>
2024-04-22 22:26:58 -0300

    chore: start migration runner tests

---
+++ tests/integration/api/v1/migrations/get.test.js
@@ -0,0 +1,13 @@
+test("GET to /api/v1/status should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+});
