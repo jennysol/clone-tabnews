e81532c1a182758f20f3ceac67a8cd0e84a49497
jennysol <jennyssoliver@gmail.com>
2024-05-07 08:57:30 -0300

    chore: add methods response to migrations index

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,12 +2,30 @@ import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 
 export default async function migrations(request, response) {
-  const migrations = await migrationRunner({
-    databaseUrl: process.env.DATABASE_URL,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-  });
-  response.status(200).json(migrations);
+  const defaultMigrationOptions = {
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+  }
+
+  if (request.method === "GET") {
+    const migrations = await migrationRunner(defaultMigrationOptions);
+    return response.status(200).json(migrations);
+  }
+
+  if (request.method === "POST") {
+    const migrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+    });
+    return response.status(200).json(migrations);
+  }
+
+  if (migrations.length > 0) {
+    return response.status(201).end();
+  }
+
+  return response.status(200).end();
 }
