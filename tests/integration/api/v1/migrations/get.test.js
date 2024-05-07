56beb8b7f9680bf006ad6e4a20ee5b04dac83998
jennysol <jennyssoliver@gmail.com>
2024-05-06 22:09:11 -0300

    feat: config database tests

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,4 +1,10 @@
-import database from "../../../../../infra/database";
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
 
 test("GET to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,3 +1,11 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
 test("POST to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
@@ -7,4 +15,6 @@ test("POST to /api/v1/migrations should return 200", async () => {
 
   expect(response.status).toBe(200);
   expect(Array.isArray(respondeBody)).toBe(true);
+  expect(respondeBody.length).toBeGreaterThan(0);
+  expect(respondeBody[0]).toHaveProperty("name");
 });
